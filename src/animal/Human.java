cb55358146a2b1800eaf4ed6dd3b677381ea607e
louis <louis@DESKTOP-2VE7A13>
2023-06-01 11:20:18 +0800

    Humanクラスのフィールド変数のカプセル化

--- src/animal/Human.java
+++ src/animal/Human.java
@@ -15,6 +15,14 @@ public class Human extends Animal implements Thinkable{
 	}
 
 
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
 	@Override
 	public void think() {
 		System.out.println("私は" + hobby + "について考えています。");
